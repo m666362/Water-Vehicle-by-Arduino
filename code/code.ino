4ee2bf676327795f0cfb5f3217b33de36a41e061
Md Rayhan <mdrayhan9464@gmail.com>
2020-09-09 13:02:10 +0600

    requiredValue from Serialprint

--- code/code.ino
+++ code/code.ino
@@ -7,21 +7,13 @@ Servo servoCW;
 Servo servoCCW;
 
 bool verticalMode = true;
+bool puttedValue = false;
+int requiredDepth;
+
 byte servoPinCW = 6;
 byte servoPinCCW = 9;
 int signalCW = 0;
 int signalCCW = 0;
-
-// int requiredDepth = 127;
-// int currentDepth = 0;
-/*
- * int sensorValue = 0;
-int outputValue = 0;
-int waterHeight = 2;
-*/
-// const int verticalThrusterCW = 6;
-// const int verticalThrusterCCW = 9;
-const int sensorPin = A0;
 
 void setup(){
   Serial.begin(9600);
@@ -51,14 +43,17 @@ void loop(){
   */
   if(verticalMode == true){
     // Write code for up-down movement
-    // sensorValue = analogRead(sensorPin);
-    // outputValue = map(sensorValue, 0, 1023, 0, 255);
     sensor.read();
-    if(sensor.depth()<=0.5){
+    Serial.print("Enter required depth: ");
+    while(Serial.available()== 0  && puttedValue == false){
+      // Do Nothing
+      Serial.print("You haven't entered value yet");
+    }
+    requiredDepth = Serial.parseInt();
+    puttedValue = true;
+    if(sensor.depth()<=requiredDepth){
       // 256/2*0.5 = 64 for 0.5 meter
       // motor will rotate so that it will float at 0.5m.
-      // analogWrite(verticalThrusterCW, 116);
-      // analogWrite(verticalThrusterCCW, 84);
       signalCW = 1718;
       signalCCW = 1246;
       servoCW.writeMicroseconds(signalCW);
@@ -66,27 +61,18 @@ void loop(){
       Serial.print("Raw Value : ");
       Serial.print((float)sensor.depth());
       Serial.print(" meter\t");
-      // Serial.print("Input : ");
-      // Serial.print(sensorValue);
-      // Serial.print("\t");
       Serial.print("Output : ");
       Serial.print("CW-1718 And CCW-1246");
       Serial.print("\n");
     }else{
       // motor will rotate to go up
-      // I have given 220 instead of 255 because i want to move robot slowly
       signalCW = 1850;
       signalCCW = 1150;
       servoCW.writeMicroseconds(signalCW);
       servoCCW.writeMicroseconds(signalCCW);
-      // analogWrite(verticalThrusterCW, 220);
-      // analogWrite(verticalThrusterCCW, 220);
       Serial.print("Raw Value : ");
       Serial.print((float)sensor.depth());
       Serial.print(" meter\t");
-      // Serial.print("Input : ");
-      // Serial.print(sensorValue);
-      // Serial.print("\t");
       Serial.print("Output : ");
       Serial.print("CW-1850 And CCW-1150");
       Serial.print("\n");
